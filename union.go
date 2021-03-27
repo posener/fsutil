642b163f74ea810ffa839eca9a9f04080e4bd244
Eyal Posener <posener@gmail.com>
2021-03-27 15:44:22 +0300

    Add check for invalid path according to the FS interface requirements

--- union.go
+++ union.go
@@ -5,9 +5,17 @@ import (
 	"io/fs"
 )
 
+// UnionFS implements union of multiple filesystems. When used, the first filesystem that has the
+// requested content is the one used. For example, if the same file exists in the first and second
+// given filesystem, the file in the second will be hidden by the file in the first.
 type UnionFS []fs.FS
 
+// Open returns the file from the first filesystem that does not return an ErrNotExist. If the file
+// does not exist in any filesystem, an ErrNotExist will be returned.
 func (u UnionFS) Open(name string) (fs.File, error) {
+	if !fs.ValidPath(name) {
+		return nil, &fs.PathError{Op: "open", Path: name, Err: fs.ErrInvalid}
+	}
 	for _, i := range u {
 		f, err := i.Open(name)
 		if !errors.Is(err, fs.ErrNotExist) {
